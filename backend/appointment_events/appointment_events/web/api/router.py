c045fe7f3270367806aa50d358f96e61346623c3
pradhanashwin <pradhanashwin.pradhan@gmail.com>
2024-08-19 19:39:17 +0545

    Removed unwanted routes

--- backend/appointment_events/appointment_events/web/api/router.py
+++ backend/appointment_events/appointment_events/web/api/router.py
@@ -1,8 +1,7 @@
 from fastapi.routing import APIRouter
 
-from appointment_events.web.api import echo, monitoring, events
+from appointment_events.web.api import monitoring, events
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
-api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
 api_router.include_router(events.router, tags=["events"])
