c90e89dcc52ffe050da29047ce901a0195439b28
pradhanashwin <pradhanashwin.pradhan@gmail.com>
2024-08-20 13:51:04 +0545

    added build in package

--- frontend/appointment/vite.config.ts
+++ frontend/appointment/vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig,loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
-
+// https://vitejs.dev/config/
 export default ({ mode }) => {
+
   return defineConfig({
       plugins: [react()],
+
       server: {
         proxy: {
           '/api': {
