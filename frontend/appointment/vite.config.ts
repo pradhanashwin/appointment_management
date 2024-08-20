9ca5530de41ba9d2cf8bd75a1847bdfe5677b5a8
pradhanashwin <pradhanashwin.pradhan@gmail.com>
2024-08-20 17:27:41 +0545

    removed node modules

--- frontend/appointment/vite.config.ts
+++ frontend/appointment/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-
+  console.log(process.env.VITE_PORT)
   return defineConfig({
       plugins: [react()],
 
